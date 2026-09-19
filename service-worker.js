0a5443f58bc280ef08c4338a84df4e55e09682ce
respnatformationtapissiers-ctrl <resp.nat.formationtapissiers@gmail.com>
2026-09-19 10:06:10 +0200

    Active les mises à jour automatiques

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE='rnf-v010';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request, {cache:'no-store'})
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+  );
+});
